c0efe64a266024d467ff0e04cf1505c4b856bcc4
tkachenko <olexiy.tkachenko@gmail.com>
2019-05-16 12:39:27 +0300

    auth

--- grpc/server.go
+++ grpc/server.go
@@ -5,8 +5,27 @@ import (
 	"spork/users"
 
 	codes "google.golang.org/grpc/codes"
+	metadata "google.golang.org/grpc/metadata"
 	status "google.golang.org/grpc/status"
 )
+
+func Auth(ctx context.Context) (userID users.UserID, err error) {
+	err = status.Errorf(codes.Unauthenticated, "auth failed")
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return
+	}
+	arr := md.Get("authtoken")
+	if len(arr) != 1 {
+		return
+	}
+	token := arr[0]
+	id, parseErr := users.ParseToken(token)
+	if parseErr != nil {
+		return
+	}
+	return id, nil
+}
 
 type Server struct {
 	UserService *users.Service
@@ -28,7 +47,12 @@ func (s *Server) Login(ctx context.Context, req *LoginRequest) (*LoginResponse, 
 func (*Server) CreateUser(ctx context.Context, req *CreateUserRequest) (*CreateUserResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateUser not implemented")
 }
+
 func (s *Server) FindByEmail(ctx context.Context, req *FindByEmailRequest) (*User, error) {
+	_, err := Auth(ctx)
+	if err != nil {
+		return nil, err
+	}
 	user, err := s.UserStore.UserByEmail(req.Email)
 	if err != nil {
 		return nil, err
